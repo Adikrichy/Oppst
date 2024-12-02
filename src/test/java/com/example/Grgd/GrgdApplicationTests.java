2e2c8d42cceaf539f6bef3e29939fc8cd4ea112b
Adikrichy <adil.erzhanoc.70@gmail.com>
2024-12-02 19:39:03 +0600

    Initial commit

---
+++ src/test/java/com/example/Grgd/GrgdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Grgd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrgdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
